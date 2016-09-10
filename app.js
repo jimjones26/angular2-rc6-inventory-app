d8972726c36aa172c32e9a25ba74d7bfd58fcae5
Jimmy Jones <jimjones26@gmail.com>
2016-09-10 18:24:28 -0400

    final commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ System.register(['@angular/core', '@angular/platform-browser', "@angular/platfor
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1, platform_browser_1, platform_browser_dynamic_1;
-    var InventoryApp, InventoryAppModule;
+    var Product, ProductDepartment, PriceDisplay, ProductImage, ProductRow, ProductsList, InventoryApp, InventoryAppModule;
     return {
         setters:[
             function (core_1_1) {
@@ -24,13 +24,138 @@ System.register(['@angular/core', '@angular/platform-browser', "@angular/platfor
                 platform_browser_dynamic_1 = platform_browser_dynamic_1_1;
             }],
         execute: function() {
+            /**
+             * Provides a 'Product' object
+             */
+            Product = (function () {
+                function Product(sku, name, imageUrl, department, price) {
+                    this.sku = sku;
+                    this.name = name;
+                    this.imageUrl = imageUrl;
+                    this.department = department;
+                    this.price = price;
+                }
+                return Product;
+            }());
+            /**
+             * @ProductDepartment: A component to show the breadcrumbs to a
+             * Product's department
+             */
+            ProductDepartment = (function () {
+                function ProductDepartment() {
+                }
+                ProductDepartment = __decorate([
+                    core_1.Component({
+                        selector: 'product-department',
+                        inputs: ['product'],
+                        template: "\n        <div class=\"product-department\">\n            <span *ngFor=\"let name of product.department; let i=index\">\n            <a href=\"#\">{{ name }}</a>\n            <span>{{i < (product.department.length-1) ? '>' : ''}}</span>\n            </span>\n        </div>\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductDepartment);
+                return ProductDepartment;
+            }());
+            /**
+            * @PriceDisplay: A component to show the price of a
+            * Product
+            */
+            PriceDisplay = (function () {
+                function PriceDisplay() {
+                }
+                PriceDisplay = __decorate([
+                    core_1.Component({
+                        selector: 'price-display',
+                        inputs: ['price'],
+                        template: "\n        <div class=\"price-display\">${{ price }}</div>\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], PriceDisplay);
+                return PriceDisplay;
+            }());
+            /**
+            * @ProductImage: A component to show a single Product's image
+            */
+            ProductImage = (function () {
+                function ProductImage() {
+                }
+                ProductImage = __decorate([
+                    core_1.Component({
+                        selector: 'product-image',
+                        host: { class: 'ui small image' },
+                        inputs: ['product'],
+                        template: "\n        <img class=\"product-image\" [src]=\"product.imageUrl\">\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductImage);
+                return ProductImage;
+            }());
+            /**
+             * @ProductRow: A component for the view of single Product
+             */
+            ProductRow = (function () {
+                function ProductRow() {
+                }
+                ProductRow = __decorate([
+                    core_1.Component({
+                        selector: 'product-row',
+                        inputs: ['product'],
+                        host: { 'class': 'item' },
+                        template: "\n        <product-image [product]=\"product\"></product-image>\n            <div class=\"content\">\n                <div class=\"header\">{{ product.name }}</div>\n                <div class=\"meta\">\n                    <div class=\"product-sku\">SKU #{{ product.sku }}</div>\n                </div>\n                <div class=\"description\">\n                    <product-department [product]=\"product\"></product-department>\n                </div>\n            </div>\n        <price-display [price]=\"product.price\"></price-display>\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductRow);
+                return ProductRow;
+            }());
+            /**
+             * @ProductsList: A component for rendering all ProductRows and
+             * storing the currently selected Product
+             */
+            ProductsList = (function () {
+                function ProductsList() {
+                    this.onProductSelected = new core_1.EventEmitter();
+                }
+                ProductsList.prototype.clicked = function (product) {
+                    this.currentProduct = product;
+                    this.onProductSelected.emit(product);
+                };
+                ProductsList.prototype.isSelected = function (product) {
+                    if (!product || !this.currentProduct) {
+                        return false;
+                    }
+                    return product.sku === this.currentProduct.sku;
+                };
+                ProductsList = __decorate([
+                    core_1.Component({
+                        selector: 'products-list',
+                        inputs: [
+                            'productList'
+                        ],
+                        outputs: [
+                            'onProductSelected'
+                        ],
+                        template: "\n        <div class=\"ui items\">\n            <product-row\n            *ngFor=\"let myProduct of productList\"\n            [product]=\"myProduct\"\n            (click)='clicked(myProduct)'\n            [class.selected]=\"isSelected(myProduct)\">\n            </product-row>\n        </div>\n    "
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], ProductsList);
+                return ProductsList;
+            }());
+            /**
+             * @InventoryApp: the top-level component for our application
+             */
             InventoryApp = (function () {
                 function InventoryApp() {
+                    this.products = [
+                        new Product('MYSHOES', 'Black Running Shoes', '/resources/images/products/black-shoes.jpg', ['Men', 'Shoes', 'Running Shoes'], 109.99),
+                        new Product('NEATOJACKET', 'Blue Jacket', '/resources/images/products/blue-jacket.jpg', ['Women', 'Apparel', 'Jackets & Vests'], 239.99),
+                        new Product('NICEHAT', 'A Nice Black Hat', '/resources/images/products/black-hat.jpg', ['Men', 'Accessories', 'Hats'], 29.99)
+                    ];
                 }
+                InventoryApp.prototype.productWasSelected = function (product) {
+                    console.log('Product clicked: ', product);
+                };
                 InventoryApp = __decorate([
                     core_1.Component({
                         selector: 'inventory-app',
-                        template: ""
+                        template: "\n        <div class=\"inventory-app\">\n            <products-list\n            [productList]=\"products\"\n            (onProductSelected)=\"productWasSelected($event)\">\n            </products-list>\n        </div>\n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], InventoryApp);
@@ -42,10 +167,19 @@ System.register(['@angular/core', '@angular/platform-browser', "@angular/platfor
                 InventoryAppModule = __decorate([
                     core_1.NgModule({
                         declarations: [
-                            InventoryApp
+                            InventoryApp,
+                            ProductImage,
+                            ProductDepartment,
+                            PriceDisplay,
+                            ProductRow,
+                            ProductsList
                         ],
-                        imports: [platform_browser_1.BrowserModule],
-                        bootstrap: [InventoryApp],
+                        imports: [
+                            platform_browser_1.BrowserModule
+                        ],
+                        bootstrap: [
+                            InventoryApp
+                        ]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], InventoryAppModule);
